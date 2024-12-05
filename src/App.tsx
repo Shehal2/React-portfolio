e316c093a0edc3000d428b9bfa07cb267e7051cf
Theekshana <shehaltheekshana@gmail.com>
2024-12-05 17:01:59 +0530

    add body

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,20 @@ function App() {
   return (
    <main className="">
     <Nav />
-      <h1></h1>
+      
+    <header className='p-32 grid grid-cols-2'>
+      <img src="" alt=""/>
+      <div className="text flex flex-col items-start gap-5 justify center">
+        <div className="text-7x1 front-bold"> Hi,I'm Theekshana</div>
+        <div className="text-xl text-gray-300">
+        I'm a Software Developer.Working towards creating software that makes life easier and more meaningful.
+        </div>
+      </div>
+      
+
+    </header>
+
+
     <Footer/>
    </main>
   
--- src/componnent/nav.tsx
+++ src/componnent/nav.tsx
@@ -12,16 +12,16 @@ const Nav = () => {
   return (
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <a href=" " className="flex items-center space-x-3 rtl:space-x-reverse">
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Theekshana</span>
         </a>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <button
-            type="button"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Get started
-          </button>
+        <button type="button" onClick={() => window.open('https://drive.google.com/file/d/1kl8-bjhn3Jhdk5Tsxj4g6vMP8N_HtwMj/view?usp=sharing', '_blank')}
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+         >
+        Download CV
+        </button>
+
           <button
             data-collapse-toggle="navbar-sticky"
             type="button"
