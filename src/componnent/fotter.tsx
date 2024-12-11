cb88769968093c0594ee605635fde86bfa88da7c
Theekshana <shehaltheekshana@gmail.com>
2024-12-11 15:14:50 +0530

    update code

--- src/componnent/fotter.tsx
+++ src/componnent/fotter.tsx
@@ -2,16 +2,15 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="bg-white rounded-none shadow">
+        <footer className="bg-gray-100 rounded-none shadow-md">
             <div className="w-full mx-0 p-4 md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between container mx-auto">
                     <a href="" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                       
                         <span className="self-center text-2xl font-semibold whitespace-nowrap text-black">Theekshana</span>
                     </a>
                     <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-black sm:mb-0">
                         <li>
-                            <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                            <a href="#about" className="hover:underline me-4 md:me-6">About</a>
                         </li>
                         <li>
                             <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
@@ -20,7 +19,7 @@ const Footer = () => {
                             <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
                         </li>
                         <li>
-                            <a href="#" className="hover:underline">Contact</a>
+                            <a href="#contact" className="hover:underline">Contact</a>
                         </li>
                     </ul>
                 </div>
